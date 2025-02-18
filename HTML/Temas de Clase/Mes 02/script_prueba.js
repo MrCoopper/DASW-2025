23afb5ddd236d75330676a04b6741767ea96ed12
MrCooper <jorges.sanchez@iteso.mx>
2025-02-17 18:54:58 -0600

    Actualizacion

---
+++ HTML/Temas de Clase/Mes 02/script_prueba.js
@@ -0,0 +1,20 @@
+
+// Forma de poder declarar variables
+let first_Var = 10;
+
+// Forma de poder declarar una variable constante
+const first_ConstVAR = 1;
+
+// Forma de poder imprimir algo con un format
+console.log(`El valor se la primera variable es de ${first_Var}`)
+
+/*
+function ejemploFunction () {
+    if (true) {
+        let x = 10;
+    }
+    console.log(x);
+}
+
+ejemploFunction();
+*/
