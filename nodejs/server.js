308101990cb533db2eb6c5e8add9bdbe5f4fedff
MrCooper <jorges.sanchez@iteso.mx>
2025-03-24 21:35:45 -0600

    Actualizacion

--- nodejs/server.js
+++ nodejs/server.js
@@ -16,6 +16,15 @@ app.get('/products/:id', (req, res) => {
 
 app.get('/products', (req, res) => {
     const queryParams = req.query;
+    if (queryParams.name) {
+        const products = JSON.parse(fs.readFileSync('products.json', 'utf-8'));
+        const product = products.find(product => product.name === queryParams.name)
+        if (!product) {
+            return res.status(404).json({error: 'Producto no encontrado'});
+        }
+    } else {
+        res.json(products);
+    }
 })
 
 app.listen(port, () => {
